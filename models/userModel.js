4e8b3383ea62ad3ec14515156bb705d9eeeba47b
noaMiller853 <128284076+noaMiller853@users.noreply.github.com>
2023-06-28 13:26:23 +0300

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -3,11 +3,14 @@ const userSchema =new mongoose.Schema({
     name: String,
     email: String,
     password: String,
+    active:Boolean,
+    tel:String,
     date_created: {
         type: Date, default: Date.now()
     },
     role: {
         type: String, default: "user"
     }
+    
 })
 exports.UserModel = mongoose.model("users", userSchema);
