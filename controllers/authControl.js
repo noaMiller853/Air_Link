45001eaae78f699eff0ab20ed727cd623824c1c2
noaMiller853 <128284076+noaMiller853@users.noreply.github.com>
2023-06-28 13:32:26 +0300

    Update authControl.js

--- controllers/authControl.js
+++ controllers/authControl.js
@@ -13,7 +13,7 @@ exports.authCtrl = {
       let user = new UserModel(req.body);
 
       user.password = await bcrypt.hash(user.password, 10);
-// We want to encrypt the password unidirectionally
+      // We want to encrypt the password unidirectionally
       await user.save();
       user.password = "***";
       res.status(201).json(user);
@@ -30,24 +30,32 @@ exports.authCtrl = {
   ,
   login: async (req, res) => {
     let validBody = loginUser(req.body);
+    let flagEmail=true;
+    let flagPassword=true;
     if (validBody.error) {
-      // .details -> Pig in detail what the client side problem is
       return res.status(400).json(validBody.error.details);
     }
     try {
-      // First of all check if the sent email exists in the database
       let user = await UserModel.findOne({ email: req.body.email })
       if (!user) {
-        return res.status(401).json({ msg: "Password or email is worng ,code:1" })
+        flagEmail=false;
+        
       }
-      // If the password sent in Buddy matches the encrypted password in that user's database
+    
       let authPassword = await bcrypt.compare(req.body.password, user.password);
       if (!authPassword) {
-        return res.status(401).json({ msg: "Password or email is worng ,code:2" });
+        flagPassword=false;
+       
       }
-      // Generate a token that contains the user's ID
-      let token = createToken(user._id, user.role);
-      res.json({ token });
+      if (!flagEmail || !flagPassword && !(!flagEmail && !flagPassword)) {
+        let tel = await UserModel.findOne({ tel: req.body.tel })
+        if (!tel) {
+          return res.status(401).json({ msg: "You need to register on the site" })
+        }
+        
+      }
+      res.status(201).json(user);
+
     }
     catch (err) {
       console.log(err)
