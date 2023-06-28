6a07263569744f18ba00d027211abc048fd30745
noaMiller853 <128284076+noaMiller853@users.noreply.github.com>
2023-06-28 13:30:54 +0300

    Update validUser.js

--- validation/validUser.js
+++ validation/validUser.js
@@ -1,16 +1,19 @@
-const Joi=require("joi")
+const Joi = require("joi")
 exports.validUser = (_reqBody) => {
     const joiSchema = Joi.object({
         name: Joi.string().min(2).max(99).required(),
         email: Joi.string().min(2).max(99).email().required(),
-        password: Joi.string().min(3).max(99).required()
+        password: Joi.string().min(3).max(99).required(),
+        tel: Joi.string().min(10).max(10).required(),
+        active: Joi.boolean().required()
     })
     return joiSchema.validate(_reqBody);
 }
-exports.loginUser=(_reqBody)=>{
-    const joiSchema=Joi.object({
+exports.loginUser = (_reqBody) => {
+    const joiSchema = Joi.object({
         email: Joi.string().min(2).max(99).email().required(),
-        password: Joi.string().min(3).max(99).required()
+        password: Joi.string().min(3).max(99).required(),
+        tel: Joi.string().min(10).max(10)
     })
     return joiSchema.validate(_reqBody);
 }
