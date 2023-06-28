995c0b4d9a780f2737079692144c29cbc09b4533
noaMiller853 <128284076+noaMiller853@users.noreply.github.com>
2023-06-28 13:29:05 +0300

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -4,19 +4,14 @@ const {authCtrl  } = require("../controllers/authControl");
 const { userCtrl } = require("../controllers/userControl");
 const router = express.Router();
 
-// An area that returns the user's information based on the token they send
+
  router.get("/myInfo",auth,userCtrl.myInfo)
 router.get("/",userCtrl.getUrl)
-// // Only an admin user will be able to reach and view the list of all users
- router.get("/usersList",authAdmin ,userCtrl.userList)
-
-router.post("/",authCtrl.signUp)
-
+router.get("/usersList",authAdmin ,userCtrl.userList)
 router.post("/login", authCtrl.login)
-
 router.put("/:idEdit",auth,userCtrl.editUser);
-
 router.delete("/:idDel" ,auth, userCtrl.deleteAccount);
+router.post("/signUp",authCtrl.signUp)
 
 
 
