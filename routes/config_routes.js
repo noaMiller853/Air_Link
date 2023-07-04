8d4472b31ec60c0a2c35216d48e8048d38b16f5f
noaMiller853 <128284076+noaMiller853@users.noreply.github.com>
2023-07-04 18:30:15 +0300

    Update config_routes.js

--- routes/config_routes.js
+++ routes/config_routes.js
@@ -4,7 +4,8 @@ const drone = require("./drone");
 const charging_station = require("./charging_station")
 const parking_station = require("./parking_station");
 const user = require("./user");
-const order=require("./order")
+const order=require("./order");
+const products=require("./product");
 
 exports.routesInit = (app) => {
     app.use("/", indexR);
@@ -13,5 +14,6 @@ exports.routesInit = (app) => {
     app.use("/drone", drone);
     app.use("/order",order);
     app.use("/parking_station",parking_station);
-    app.use("/charging_station", charging_station)
+    app.use("/charging_station", charging_station);
+    app.use("/products",products);
 }
