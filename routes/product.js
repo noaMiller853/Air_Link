458572b6d85cc1a4f6ea3bbb7ea8d48de06c6a94
noaMiller853 <128284076+noaMiller853@users.noreply.github.com>
2023-07-04 17:34:48 +0300

    Create product.js

---
+++ routes/product.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const { ProductModel } = require("../models/productModel");
+const router = express.Router();
+
+// GET all products
+router.get('/products', async (req, res) => {
+  try {
+    const products = await ProductModel.find({});
+    res.json(products);
+  } catch (err) {
+    console.error('Failed to fetch products', err);
+    res.status(500).json({ error: 'Failed to fetch products' });
+  }
+});
+
+// GET a single product by ID
+router.get('/products/:id', async (req, res) => {
+  try {
+    const product = await ProductModel.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
+    res.json(product);
+  } catch (err) {
+    console.error('Failed to fetch product', err);
+    res.status(500).json({ error: 'Failed to fetch product' });
+  }
+});
+
+// POST a new order
+router.post('/productsOfOrder', async (req, res) => {
+  const { name, email, productId,weight } = req.body;
+
+  // Validate the request body
+  if (!name || !email || !productId||weight) {
+    return res.status(400).json({ error: 'Missing required fields' });
+  }
+
+  try {
+    // Check if the product exists
+    const product = await ProductModel.findById(productId);
+    if (!product) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
+
+    // Save the order to the database
+    // You can define an Order model and create a new order here
+
+    res.json({ success: true });
+  } catch (err) {
+    console.error('Failed to save order', err);
+    res.status(500).json({ error: 'Failed to save order' });
+  }
+});
+module.exports=router;
+
